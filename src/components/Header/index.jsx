9a5b44878ea49394bc95c3bfeb8e2eaeba56227c
Marcelo Saldanha <marcelo.sfilho97@gmail.com>
2025-08-27 12:41:58 -0300

    Erros a resolver no darkmode component header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,39 @@
+import { useEffect, useState } from "react";
 import ImageSwitch from "./assets/Switch.svg";
 import ImgProfile from "./assets/img-profile.png";
 import "./style.css";
 
 export default function Header () {
+
+  const darkmode = {
+    background: "--bg-variant02",
+    status: 1,
+  }
+  const lightmode = {
+    background: "--bg-variant01", 
+    status: 0,
+  }
+  const myimage = document.getElementsByClassName('.modeImage');
+  const [Mode, setMode] = useState([]);
+  let currentMode = Mode.status;
+  console.log(currentMode);
+
   return (
     <header className="Profile-header">
       <img className="img-profile" src={ImgProfile} alt="Foto de Perfil" />
       <span>Marcelo Saldanha</span>
-      <button>
-        <img src={ImageSwitch} alt="Botão Dark Mode"/>
+      <button onClick={(currentMode) => {
+        currentMode = Mode.status === 0 ? setMode(darkmode) : setMode(lightmode);
+
+        /*
+        <img 
+        className="modeImage" 
+        src={ImageSwitch} 
+        alt="Botão Dark Mode"/>
+        */
+      } }className="btn-switch">
       </button>
     </header>
   )
 }
+
