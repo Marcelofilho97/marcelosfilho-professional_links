545142f61f591e2f60f8ad896a0fda15f867348e
Marcelo Saldanha <marcelo.sfilho97@gmail.com>
2025-08-28 12:05:22 -0300

    botão dark mode funcionando

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,37 +3,58 @@ import ImageSwitch from "./assets/Switch.svg";
 import ImgProfile from "./assets/img-profile.png";
 import "./style.css";
 
-export default function Header () {
-
-  const darkmode = {
-    background: "--bg-variant02",
-    status: 1,
-  }
-  const lightmode = {
-    background: "--bg-variant01", 
-    status: 0,
-  }
-  const myimage = document.getElementsByClassName('.modeImage');
-  const [Mode, setMode] = useState([]);
-  let currentMode = Mode.status;
-  console.log(currentMode);
+export default function Header () {  
+  const [Mode, setMode] = useState(false);
+  let currentMode = Mode;
 
   return (
     <header className="Profile-header">
       <img className="img-profile" src={ImgProfile} alt="Foto de Perfil" />
       <span>Marcelo Saldanha</span>
-      <button onClick={(currentMode) => {
-        currentMode = Mode.status === 0 ? setMode(darkmode) : setMode(lightmode);
-
-        /*
+      <button
+        onClick={() => {
+          currentMode ? currentMode = setMode(false) : currentMode = setMode(true);
+        }}
+        className="btn-switch">
         <img 
-        className="modeImage" 
-        src={ImageSwitch} 
-        alt="Botão Dark Mode"/>
-        */
-      } }className="btn-switch">
+          className="modeImage" 
+          src={ImageSwitch}
+          style={ currentMode ? {
+            filter: 'invert(1)',
+            transform: 'scaleX(-1)'
+          }: {
+            filter: 'invert(0)',
+            transform: 'scaleX(1)'
+          } }
+          alt="Botão Dark Mode"/>
       </button>
     </header>
   )
 }
 
+/*
+  onClick= {
+        // setMode(true);
+        //(currentMode) => {
+        //currentMode = Mode.status === 0 ? setMode(darkmode) : setMode(lightmode);
+        // criação do elemento imagem;
+        
+        //const myImage = document.createElement('img');
+        //if (currentMode === false) {
+          //setMode(lightmode);
+          //const myImage = document.createElement('img');
+          //myImage.src = ImageSwitch;
+          //myImage.className = 'modeImage';
+          //myImage.style.setProperty('filter', 'invert(0)');
+          //console.log("funfou");
+        //} else {
+          //setMode(darkmode);
+          //myImage.style.setProperty('filter', 'invert(0)');
+        //}
+        /*
+        <img 
+        className="modeImage" 
+        src={ImageSwitch} 
+        alt="Botão Dark Mode"/>
+     }
+     */
